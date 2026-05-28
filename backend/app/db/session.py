05d704c0971b923a6bd7c764998d929d17c6429f
GayatriRekner <reknergayatri@gmail.com>
2026-05-28 11:51:01 +0530

    fix session config

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
+import os
+load_dotenv()
 
-DATABASE_URL = "mysql+pymysql://root:Gayatri%400123@localhost/code_review_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,
